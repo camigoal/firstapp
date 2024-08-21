f6b87e2fef2e54f5de48ef4b7b6bf6ec42fea88b
camigoal <camila.gomeza@uniagustiniana.edu.co>
2024-08-21 15:48:49 -0500

    quinto commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
-
+import Nav from './components/Nav';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Promo from './components/Promo';
+import Footer from './components/Footer';
 
 function App() {
+  return (
+    <div>
+    <Nav/><Promo/><Intro1/><Intro2/><Intro3/><Footer/>
+    </div>
+  );
 }
 
 export default App;
