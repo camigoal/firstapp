a294425701b737338abd85d0773e3214240014ea
camigoal <camila.gomeza@uniagustiniana.edu.co>
2024-08-21 13:08:00 -0500

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Heading from './Heading';
-import Header from './Header';
 
-// function Header() {
-//   return <h1>Hello world</h1>;
-// }
+
 
 function App() {
-  return <Header/>; 
 }
 
 export default App;
