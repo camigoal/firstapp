dd548877eb754688c9a00bd971a03cc21170f698
camigoal <camila.gomeza@uniagustiniana.edu.co>
2024-08-23 12:49:02 -0500

    navbar update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-function Nav() {
+function Nav(props) {
     return (
         <nav className="main-nav">
             <ul>
-                <li>Home</li>
+                {/* <li>Home</li>
                 <li>Articles</li>
                 <li>About</li>
-                <li>Contact</li>
+                <li>Contact</li> */}
+                <li>{props.first}</li>
+                <li>{props.second}</li>
+                <li>{props.third}</li>
+                <li>{props.fourth}</li>
             </ul>
         </nav>
     );
-};
+}
 
 export default Nav;
